d14b6b38a6548451c70b156d3e43d6925f25dc42
CharlesHsu <charles040318@gmail.com>
2025-08-21 00:50:19 +0800

    add

---
+++ dubhe_mgi/launch/dubhe_mgi_left.launch.py
@@ -0,0 +1,35 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from moveit_configs_utils import MoveItConfigsBuilder
+
+
+def generate_launch_description():
+    ns_arg = DeclareLaunchArgument("ns", default_value="")
+    group_arg = DeclareLaunchArgument("planning_group", default_value="left_arm")
+    ref_arg = DeclareLaunchArgument("reference_frame", default_value="lift")
+
+    ns = LaunchConfiguration("ns")
+    group = LaunchConfiguration("planning_group")
+    ref = LaunchConfiguration("reference_frame")
+
+    moveit_config = (
+        MoveItConfigsBuilder("rm_75_dual", package_name="rm_75_config")
+        .planning_pipelines(pipelines=["ompl"])
+        .to_moveit_configs()
+    )
+
+    client = Node(
+        package="dubhe_mgi",
+        executable="dubhe_mgi",
+        namespace=ns,
+        output="screen",
+        parameters=[
+            moveit_config.to_dict(),
+            {"planning_group": group, "reference_frame": ref, "robot_ns": ns},
+        ],
+        # arguments=["--ros-args", "--log-level", "debug"],
+    )
+
+    return LaunchDescription([ns_arg, group_arg, ref_arg, client])
